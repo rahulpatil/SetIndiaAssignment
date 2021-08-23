8f5c6705e603481325078676d17d8e6ca7558d19
rahulpatil <rspatil88@gmail.com>
2021-08-23 12:16:10 +0530

    Create view-model (#5)
    Download data from remote URL
    
    Co-authored-by: Rahul Patil <rahul.patil@mediakind.com>

--- app/src/main/java/com/setindia/task/MainActivity.kt
+++ app/src/main/java/com/setindia/task/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.setindia.task
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.setindia.task.viewmodel.LanguageViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var languageViewModel: LanguageViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViewModel()
+
+    }
+
+    private fun initViewModel() {
+        languageViewModel = ViewModelProvider(this).get(LanguageViewModel::class.java)
+        languageViewModel.getLanguageMap()
     }
 }
---
+++ app/src/main/java/com/setindia/task/repository/RemoteRepository.kt
@@ -0,0 +1,81 @@
+package com.setindia.task.repository
+
+import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.*
+import java.nio.charset.Charset
+
+const val TAG = "RemoteRepository"
+const val downloadFileUrl =
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRgAkuJOteBzfIBCuedMAFxIOXonw012HIVAMkzWmTzCDBQ62yUbXj3_wGbjVIQAjhmidH62239jJar/pub?gid=273134001&single=true&output=csv"
+
+class RemoteRepository {
+
+    companion object {
+
+        private var languageMap: HashMap<String, List<String>> = HashMap()
+
+        fun downloadTranslatorFile(): HashMap<String, List<String>> {
+
+            var stream: InputStream? = null
+
+            try {
+
+                val url = URL(downloadFileUrl)
+                val con: URLConnection = url.openConnection()
+
+                stream = con.getInputStream()
+
+                val bufferedReader = BufferedReader(
+                    InputStreamReader(stream, Charset.forName("UTF-8"))
+                )
+
+                var line: String?
+
+                do {
+
+                    line = bufferedReader.readLine()
+
+                    if (line == null)
+                        break
+
+                    val columns: List<String> = line.split(",")
+
+                    languageMap[columns[0]] = columns.subList(1, columns.size)
+
+
+                } while (true)
+
+                return languageMap
+
+            } catch (e: MalformedURLException) {
+                Log.e(TAG, " MalformedURLException")
+                e.printStackTrace()
+            } catch (se: SocketTimeoutException) {
+                Log.e(TAG, "SocketTimeoutException")
+                se.printStackTrace()
+            } catch (e: IOException) {
+                Log.e(TAG, "IOException")
+                e.printStackTrace()
+            } catch (u: URISyntaxException) {
+                Log.e(TAG, "URISyntaxException")
+                u.printStackTrace()
+            } catch (ne: NullPointerException) {
+                Log.e(TAG, "NullPointerException cannot retrieve URL")
+                ne.printStackTrace()
+            } finally {
+                try {
+                    stream?.close()
+                } catch (e: IOException) {
+                    Log.e(TAG, "stream close", e.cause)
+                }
+            }
+
+            return languageMap
+
+        }
+    }
+}
