34cd365aca9814946ace32e2afff27637fa7977c
rahulpatil <rspatil88@gmail.com>
2021-08-23 14:39:16 +0530

    Add button click handling (#6)
    Co-authored-by: Rahul Patil <rahul.patil@mediakind.com>

--- app/src/main/java/com/setindia/task/MainActivity.kt
+++ app/src/main/java/com/setindia/task/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.setindia.task
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.setindia.task.repository.TAG
 import com.setindia.task.viewmodel.LanguageViewModel
+
+const val LANGUAGE_CODE_ENG = "en_IN"
+const val LANGUAGE_CODE_CHN = "zh_CN"
+const val LANGUAGE_CODE_HIN = "hi_IN"
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         initViewModel()
 
     }
@@ -22,4 +27,22 @@ class MainActivity : AppCompatActivity() {
         languageViewModel = ViewModelProvider(this).get(LanguageViewModel::class.java)
         languageViewModel.getLanguageMap()
     }
+
+    fun setEnglish(view: View) {
+        translateAndSetText(LANGUAGE_CODE_ENG)
+    }
+
+    private fun translateAndSetText(languageCode: String) {
+        val languageCodeIndex = languageViewModel.getLanguageIndex(languageCode)
+        Log.d(TAG, "The index of selected laguage is $languageCode")
+    }
+
+    fun setChinese(view: View) {
+        translateAndSetText(LANGUAGE_CODE_CHN)
+    }
+
+    fun setHindi(view: View) {
+        translateAndSetText(LANGUAGE_CODE_HIN)
+    }
+
 }
--- app/src/main/java/com/setindia/task/viewmodel/LanguageViewModel.kt
+++ app/src/main/java/com/setindia/task/viewmodel/LanguageViewModel.kt
@@ -11,21 +11,18 @@ const val RESOURCE_ID = "resource_id" // Column header for String id
 class LanguageViewModel(application: Application) : AndroidViewModel(application) {
 
     //TODO :- Convert to LIVE data
-    private var myMap: HashMap<String, List<String>>? = HashMap()
+    // Improve this data structure,write model to getId based on TAG
+    private var languageMap: HashMap<String, List<String>> = HashMap()
 
-     fun getLanguageMap() {
-         thread(start = true) {
-             myMap = RemoteRepository.downloadTranslatorFile()
-         }
-     }
+    fun getLanguageMap() {
+        thread(start = true) {
+            languageMap = RemoteRepository.downloadTranslatorFile()
+        }
+    }
 
     //TODO :- Move to Model class once created
     fun getLanguageIndex(languageCode: String?): Int {
-        return myMap?.get(RESOURCE_ID)?.indexOf(languageCode) ?: 1
-    }
-
-    fun getTranslatedString(value:String, index:Int):String? {
-        return myMap?.get(value)?.get(index)
+        return languageMap[RESOURCE_ID]?.indexOf(languageCode) ?: 1
     }
 
 }
