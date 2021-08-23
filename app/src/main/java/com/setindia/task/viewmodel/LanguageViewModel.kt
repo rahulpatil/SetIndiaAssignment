e8ebd02c2cc522d9da33d718af11c4c64f54daae
Rahul Patil <rahul.patil@mediakind.com>
2021-08-23 15:34:06 +0530

    Build translation logic

--- app/src/main/java/com/setindia/task/viewmodel/LanguageViewModel.kt
+++ app/src/main/java/com/setindia/task/viewmodel/LanguageViewModel.kt
@@ -1,14 +1,15 @@
 package com.setindia.task.viewmodel
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import android.util.Log
+import androidx.annotation.VisibleForTesting
+import androidx.lifecycle.ViewModel
 import com.setindia.task.repository.RemoteRepository
 import kotlin.concurrent.thread
 
 const val RESOURCE_ID = "resource_id" // Column header for String id
 
 //ViewModel is optional, can be avoided . Added for future enhancements
-class LanguageViewModel(application: Application) : AndroidViewModel(application) {
+class LanguageViewModel : ViewModel() {
 
     //TODO :- Convert to LIVE data
     // Improve this data structure,write model to getId based on TAG
@@ -17,12 +18,35 @@ class LanguageViewModel(application: Application) : AndroidViewModel(application
     fun getLanguageMap() {
         thread(start = true) {
             languageMap = RemoteRepository.downloadTranslatorFile()
+            Log.d("Rahul", "nap is $languageMap")
         }
     }
 
     //TODO :- Move to Model class once created
     fun getLanguageIndex(languageCode: String?): Int {
         return languageMap[RESOURCE_ID]?.indexOf(languageCode) ?: 1
+    }
+
+    fun getTranslatedString(text: String, index: Int): String {
+        return languageMap[getMapKey(text)]?.get(index) ?: text
+    }
+
+    //TODO :- Ideally if we get resource name of String Set to view, then this logic not required
+    private fun getMapKey(text: String): String? {
+        for ((key, valueList) in languageMap.entries) {
+            for (value in valueList) {
+                if (value == text) {
+                    return key
+                }
+            }
+
+        }
+        return null
+    }
+
+    @VisibleForTesting
+    fun setMap(map: HashMap<String, List<String>>) {
+        languageMap = map
     }
 
 }
--- app/src/test/java/com/setindia/task/ExampleUnitTest.kt
+++ app/src/test/java/com/setindia/task/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.setindia.task
 
+import com.setindia.task.repository.RemoteRepository
+import com.setindia.task.viewmodel.LanguageViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,23 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testFileDownload() {
+        val map = RemoteRepository.downloadTranslatorFile()
+        print("$map")
+
+        assertNotNull(map)
+    }
+
+    @Test
+    fun testGetLanguageIndex() {
+        val model = LanguageViewModel()
+        val map = RemoteRepository.downloadTranslatorFile()
+        assertNotNull(map)
+        model.setMap(map)
+        val index = model.getLanguageIndex("hi_IN")
+        assertEquals(3, index)
     }
 }
